276143b763a6515f842bcd710e99cc76a88757ce
unitofWelch <61781601+unitofWelch@users.noreply.github.com>
2020-12-03 21:43:10 +1000

    03.12 - 21:42, Almost done, accs on the way

--- input.php
+++ input.php
@@ -23,7 +23,7 @@
             $res .= decbin(ord($str[$i]));
         }
 
-        echo "<br>" . "Decbinned: " . $dbg_str . " - " . $res;
+//        echo "<br>" . "Decbinned: " . $dbg_str . " - " . $res;
     }
 
     function parse($alph, $content, $dbg_str)
@@ -32,7 +32,7 @@
         $c = strlen($content);
 
         $trimmed = preg_replace("/\s+/", "", $content);
-        echo "<br>" . "Trimmed: " . $dbg_str . " - " . $trimmed;
+//        echo "<br>" . "Trimmed: " . $dbg_str . " - " . $trimmed;
     }
 
     function _trim($str)
@@ -49,59 +49,117 @@
 
         $t = ceil(strlen($pt) / strlen($g));
 
-        echo "<br><br>t: " . $t;
+//        echo "<br><br>t: " . $t;
 
-        for ($j = 1; $j <= $t; $j++) {
+        $j = 0;
 
+        for ($i = 0; $i < strlen($pt); $i++) {
 
+            if ($j >= strlen($g))
+                $j = 0;
+//            echo "<br>j: " . $j . "... resetting";
 
-            for ($i = 0; $i < strlen($g); $i++) {
+            $cur_pt_sym = mb_strcut($pt, $i, 1);
+            $cur_g_sym = mb_strcut($g, $j, 1);
 
-                $cur_pt_sym = mb_strcut($pt, $i, 1);
-                $cur_g_sym = mb_strcut($g, $i, 1);
+//            echo "<br><br>cur_pt_sym: " . $cur_pt_sym;
+//            echo "<br>cur_g_sym: " . $cur_g_sym;
 
-                echo "<br><br>cur_pt_sym: " . $cur_pt_sym;
-                echo "<br>cur_g_sym: " . $cur_g_sym;
+            $cur_pt_num = array_search($cur_pt_sym, $alph);
+            $cur_g_num = array_search($cur_g_sym, $alph);
 
-                $cur_pt_num = array_search($cur_pt_sym, $alph);
-                $cur_g_num = array_search($cur_g_sym, $alph);
+//            echo "<br>cur_pt_num: " . $cur_pt_num;
+//            echo "<br>cur_g_num: " . $cur_g_num;
 
-                echo "<br>cur_pt_num: " . $cur_pt_num;
-                echo "<br>cur_g_num: " . $cur_g_num;
-
-                $result_num = $cur_pt_num + $cur_g_num;
-                while ($result_num > count($alph)) {
+            $result_num = $cur_pt_num + $cur_g_num;
+            while ($result_num > count($alph)) {
 
 //                    echo "<br>$result_num: " . $result_num;
 //                    echo "<br>count(alph): " . count($alph);
 
-                    $result_num -= count($alph);
+                $result_num -= count($alph);
 //                    echo "<br>result: " . $result;
-                }
-
-                echo "<br>result_num: " . $result_num;
-
-                echo "<br>alph: " . $alph[$result_num];
-
-                array_push($result, $alph[$result_num]);
             }
+
+//            echo "<br>result_num: " . $result_num;
+//            echo "<br>alph: " . $alph[$result_num];
+
+            array_push($result, $alph[$result_num]);
+
+            $j++;
+//            echo "<br>j: " . $j . "... normal";
 
         }
 
-        return $result;
+        return implode($result);
+    }
+
+    function ungamming($ct, $g, $alph)
+    {
+
+        $result = array();
+
+        $t = ceil(strlen($ct) / strlen($g));
+
+//        echo "<br><br>t: " . $t;
+
+        $j = 0;
+
+        for ($i = 0; $i < strlen($ct); $i++) {
+
+            if ($j >= strlen($g))
+                $j = 0;
+//            echo "<br>j: " . $j . "... resetting";
+
+            $cur_pt_sym = mb_strcut($ct, $i, 1);
+            $cur_g_sym = mb_strcut($g, $j, 1);
+
+//            echo "<br><br>cur_pt_sym: " . $cur_pt_sym;
+//            echo "<br>cur_g_sym: " . $cur_g_sym;
+
+            $cur_pt_num = array_search($cur_pt_sym, $alph);
+            $cur_g_num = array_search($cur_g_sym, $alph);
+
+//            echo "<br>cur_pt_num: " . $cur_pt_num;
+//            echo "<br>cur_g_num: " . $cur_g_num;
+
+            $result_num = $cur_pt_num - $cur_g_num;
+            while ($result_num > count($alph)) {
+
+    //                    echo "<br>$result_num: " . $result_num;
+    //                    echo "<br>count(alph): " . count($alph);
+
+                $result_num -= count($alph);
+    //                    echo "<br>result: " . $result;
+            }
+            if ($result_num < 0) $result_num = count($alph) - abs($result_num);
+
+//            echo "<br>result_num: " . $result_num;
+//            echo "<br>alph: " . $alph[$result_num];
+
+            array_push($result, $alph[$result_num]);
+
+            $j++;
+//            echo "<br>j: " . $j . "... normal";
+
+        }
+
+        return implode($result);
     }
 
     $plaintext = file_get_contents($_FILES['pt_source']['tmp_name']);
-    echo "Original PT: " . $plaintext;
+    echo "Original plain text: " . $plaintext;
     $plaintext = _trim($plaintext);
-    echo "<br>Trimmed PT: " . $plaintext;
+    echo "<br>Trimmed plain text: " . $plaintext;
 
     $gamma = file_get_contents($_FILES['g_source']['tmp_name']);
-    echo "<br><br>Original G: " . $gamma;
+    echo "<br><br>Original gamma: " . $gamma;
     $gamma = _trim($gamma);
-    echo "<br>Trimmed G: " . $gamma;
+    echo "<br>Trimmed gamma: " . $gamma;
 
-    echo "Result: " . gamming($plaintext, $gamma, $alph);
+    echo "<br><br>Gamming result: " . gamming($plaintext, $gamma, $alph);
+
+    echo "<br><br>Ungamming result: " . ungamming(gamming($plaintext, $gamma, $alph), $gamma, $alph);
 
 //    decbinned($alph, "content");
 //    decbinned($content, "content");
